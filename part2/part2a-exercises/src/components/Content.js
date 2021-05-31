030255cb363d520c5bd92c2e42a361a1d8930853
Albert Pedrola <b.albemar@gmail.com>
2021-06-01 01:11:53 +0200

    course information step8

--- part2/part2a-exercises/src/components/Content.js
+++ part2/part2a-exercises/src/components/Content.js
@@ -5,9 +5,15 @@ const Content = ({ parts }) => {
   const elm = () => parts.map(part => 
     <Part key={part.id} part={part} />
   )
+
+  const total = () => parts.reduce((s, p) => 
+    s + p.exercises, 0
+  )
+
   return (
     <div>
       {elm()}
+      <p>total of <strong>{total()} exercises</strong></p>
     </div>
   )
 }
