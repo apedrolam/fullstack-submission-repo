321de0680fef8687a584026f967eb02ea3c3ece0
Albert Pedrola <b.albemar@gmail.com>
2021-06-01 11:17:46 +0200

    part2.5

--- part2/part2a-exercises/src/components/Content.js
+++ part2/part2a-exercises/src/components/Content.js
@@ -2,18 +2,21 @@ import React from 'react'
 import Part from './Part.js'
 
 const Content = ({ parts }) => {
-  const elm = () => parts.map(part => 
-    <Part key={part.id} part={part} />
+  const rows = () => parts.map(part =>
+    <Part
+      key={part.id}
+      part={part}
+    />
   )
 
-  const total = () => parts.reduce((s, p) => 
+  const total = () => parts.reduce((s, p) =>
     s + p.exercises, 0
   )
-
+  
   return (
     <div>
-      {elm()}
-      <p>total of <strong>{total()} exercises</strong></p>
+      {rows()}
+      <p><b>total of {total()} exercises</b></p>
     </div>
   )
 }
--- part2/part2a-exercises/src/components/Course.js
+++ part2/part2a-exercises/src/components/Course.js
@@ -2,13 +2,19 @@ import React from 'react'
 import Header from './Header.js'
 import Content from './Content.js'
 
-const Course = ({ course }) => {
-    return (
-      <div>
+const Course = ({ courses }) => {
+  const course_rows = courses.map(course =>
+      <div key={course.id}>
         <Header name={course.name} />
         <Content parts={course.parts} />
       </div>
     )
+  return (
+    <>
+      <h1>Web development curriculum</h1>
+      {course_rows}
+    </>
+  )
 }
 
 export default Course;
